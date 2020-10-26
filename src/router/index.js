636a724aa5b0a500be160f6b44a4df9849304e24
Oğuz Ergül <erguloguz@hotmail.com>
2020-10-26 15:07:47 +0300

    project init
    global styles declared - related icons initalized - necessary packages installed

--- src/router/index.js
+++ src/router/index.js
@@ -11,19 +11,22 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/detail/:id',
+    name: 'Detail',
+    component: () => import(/* webpackChunkName: "about" */ '../views/Detail.vue')
   }
 ]
+
+
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
+
+router.afterEach(()=>{
+  window.scrollTo(0,0)
+});
 
 export default router
--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,63 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from "axios"
+import VuexPersistence from 'vuex-persist'
 Vue.use(Vuex)
+
+const posts_resource ="https://jsonplaceholder.typicode.com/posts";
+const put_resource ="https://jsonplaceholder.typicode.com/posts";
+const users_resources ="https://jsonplaceholder.typicode.com/users";
+
+const vuexLocal = new VuexPersistence({
+  storage: window.localStorage,
+  key: 'sikayet-var'
+})
 
 export default new Vuex.Store({
   state: {
+    posts:[],
+    users:[],
   },
   mutations: {
+    SET_POSTS(state,post) {
+      state.posts = post;
+    },
+    SET_USERS(state,payload) {
+      state.users = payload;
+    },
+    REMOVE_POSTS(state, post) {
+      const index = state.posts.findIndex((item) => item.id === post.id);
+      state.posts.splice(index, 1);
+    },
+    UPDATE_POST_ITEM(state, post) {
+      const index = state.posts.findIndex((item) => item.id === post.id);
+      const temp = [...state.posts];
+      temp[index] = post;
+      state.posts = temp;
+    },
   },
   actions: {
+    loadPosts({commit}){
+      axios.get(posts_resource)
+          .then((response)  =>{
+            commit ("SET_POSTS", response.data.splice(0,6))}
+          )
+    },
+    loadUsers({commit}){
+      axios.get(users_resources)
+          .then((response) =>{
+            commit ("SET_USERS", response.data.splice(0,6))}
+          )
+    },
+    updatePosts({ commit },payload){
+      return axios.put(put_resource + '/' + payload.id,payload);
+    },
   },
   modules: {
-  }
+  },
+  getters: {
+    getPosts:(state) => state.posts,
+    getUsers:(state) => state.users,
+  },
+  plugins: [vuexLocal.plugin]
 })
